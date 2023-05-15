6fb3d528aeaf663ffa725dc28936cec55e8fc005
Victor Inácio <chustrupgamer@gmail.com>
2023-05-15 00:25:28 -0300

    Initial Commit

---
+++ music-sheet-app/music_sheet_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_sheet_appApp.swift
+//  music-sheet-app
+//
+//  Created by Victor Soares on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct music_sheet_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
