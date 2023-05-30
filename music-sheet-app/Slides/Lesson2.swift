b2cf8676ba4758a161cb578446a63548c214cb0c
Victor Inácio <chustrupgamer@gmail.com>
2023-05-30 00:24:42 -0300

    Ädicionado mais uma aula

---
+++ music-sheet-app/Slides/Lesson2.swift
@@ -0,0 +1,20 @@
+//
+//  Lesson2.swift
+//  music-sheet-app
+//
+//  Created by Victor Soares on 29/05/23.
+//
+
+import SwiftUI
+
+struct Lesson2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Lesson2_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson2()
+    }
+}
